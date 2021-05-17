b13667bad18acdd8be0da6472401297791c81f95
NDJoseph6078 <nathanjoseph6078@gmail.com>
2021-05-17 15:19:57 +0100

    Added form validation

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // // Function that gets the user details input in the form when the button with the id of storeTaskBtn is clicked
 document.querySelector('#storeTaskBtn').addEventListener('click', function () {
     
-   let assignedBy = document.querySelector('#inputAssignedBy').value;
-   let description = document.querySelector('#inputDesc').value;
-   let assignedTo = document.querySelector('#inputAssignedTo').value;
-   let dueDate = document.querySelector('#inputDueDate').value;
-   let status = document.querySelector('#inputStatus').value;
+   let assignedBy = document.querySelector('#inputAssignedBy').value.trim();
+   let description = document.querySelector('#inputDesc').value.trim();
+   let assignedTo = document.querySelector('#inputAssignedTo').value.trim();
+   let dueDate = document.querySelector('#inputDueDate').value.trim();
+   let status = document.querySelector('#inputStatus').value.trim();
 
-   createTaskCard(assignedBy, description, assignedTo, dueDate, status)
+   if (isObjectEmpty(validateTaskForm(assignedBy, description, assignedTo, dueDate))){
+    createTaskCard(assignedBy, description, assignedTo, dueDate, status);  
+   } else{
+    console.log("test");
+   }
+   
 
 //    storeTaskDetails(assignedBy, description, assignedTo, dueDate, status)
 });
@@ -30,46 +35,75 @@ function createTaskCard(assignedBy, description, assignedTo, dueDate, status){
     taskCard = document.getElementById("taskCards");
     taskCard.innerHTML += ` <div class="col-12 col-sm-6 col-lg-4">
     <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action ">
+        <div  class="list-group-item list-group-item-action ">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Task</h5>
             <small>3 days ago</small>
           </div>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        </div>
+        <div  class="list-group-item list-group-item-action">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Assigned By</h5>
           </div>
           <p class="mb-1">${assignedBy}</p>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        </div>
+        <div  class="list-group-item list-group-item-action">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Description</h5>
           </div>
           <p class="mb-1">${description}</p>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        </div>
+        <div  class="list-group-item list-group-item-action">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Assigned To</h5>
           </div>
           <p class="mb-1">${assignedTo}</p>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        </div>
+        <div  class="list-group-item list-group-item-action">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Due Date</h5>
           </div>
           <p class="mb-1">${dueDate}</p>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
+        </div>
+        <div  class="list-group-item list-group-item-action">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">Status</h5>
           </div>
           <p class="mb-1">${status}</p>
-        </a>
+        </div>
         
       </div>`;
 
+};
+
+function isObjectEmpty(obj){
+    console.log(Object.keys(obj));
+    return Object.keys(obj).length === 0;
 }
+
+function validateTaskForm(assignedBy, description, assignedTo, dueDate){
+    const errors = {};
+    const specialChars = /[`!@#£$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const numberList = /[1234567890]/;
+    const dateFormat =/[dmy]/ 
+    if ((assignedBy.length > 20) || (assignedBy.length < 1) || (specialChars.test(assignedBy) == true) || (numberList.test(assignedBy) == true)) {
+        errors["Assigned By"] = "The length of this input has to be between 1 and 20 characters, and have no special characters ";
+    };
+    if ((description.length > 20) || (description.length < 1) || (specialChars.test(description) == true)) {
+        errors["Description"] = "The length of this input has to be between 1 and 20 characters, and have no special characters";
+    };
+    if ((assignedTo.length > 20) || (assignedTo.length < 1) || (specialChars.test(assignedTo) == true) || (numberList.test(assignedTo) == true)) {
+        errors["Assigned To"] = "The length of this input has to be between 1 and 20 characters, and have no special characters";
+    };
+    if (dueDate.length < 10) {
+        errors["Due Date"] = "The Date has no been formatted correctly";
+    };
+    console.log(errors);
+    return errors;
+    
+};
+
+
 
 // let taskArray = [];
 
