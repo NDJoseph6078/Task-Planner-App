b7f18185601bb4284f1e78d1e6e24c565e860831
NDJoseph6078 <nathanjoseph6078@gmail.com>
2021-05-18 17:08:07 +0100

    Added a counter that is saved in local storage and updated whenever a new card is made so each card will have a unique id which allows for my summary card to link to the full card at the bottom

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 if (!localStorage.getItem("taskArray")){
   localStorage.setItem("taskArray", "[]")
 }
-
+if (!localStorage.getItem("counter")){
+  localStorage.setItem("counter", "0")
+}
 
 
 
@@ -18,7 +20,7 @@ document.querySelector('#storeTaskBtn').addEventListener('click', function () {
   //Checks if the an array of the object's keys is empty, in this case this is useful for the checking the 'error' object that is returned from the validateTaskForm function because if the array is empty it means the user data input did that did not engage with any of the conditions within the validateTaskForm function meaning that the data input was formatted correctly. 
   if (isObjectEmpty(validateTaskForm(assignedBy, description, assignedTo, dueDate))){
     storeTaskDetails(assignedBy, description, assignedTo, dueDate, status); 
-    location.reload(); //Forces the page to reload
+    //location.reload(); //Forces the page to reload
   } else{
     console.log("Card cannot be created as a field is empty or formatted incorrectly");
   }
@@ -28,26 +30,30 @@ document.querySelector('#storeTaskBtn').addEventListener('click', function () {
 // Function to store the details input by the user in local storage for it to later be used
 function storeTaskDetails(assignedBy, description, assignedTo, dueDate, status){
     let taskObject = new Task(assignedBy, description, assignedTo, dueDate, status);
+    console.log(taskObject.taskID)
+    let idCounter = taskObject.taskID;
     let taskArray = JSON.parse(localStorage.getItem("taskArray"));
     taskArray.push(taskObject);
     localStorage.setItem("taskArray", JSON.stringify(taskArray));
+    localStorage.setItem("counter", (++idCounter))
 };
 
 class Task {
+  
   constructor (assignedBy, description, assignedTo, dueDate, status){
     this.taskAssignedBy = assignedBy;
     this.taskDescription = description;
     this.taskAssignedTo = assignedTo;
     this.taskDueDate = dueDate;
     this.taskStatus = status;
+    this.taskID = parseInt(localStorage.getItem("counter"));
   }
-
 }
 
 // Function that creates the individual cards under the form
-function createTaskCard(assignedBy, description, assignedTo, dueDate, status){
+function createTaskCard(assignedBy, description, assignedTo, dueDate, status, taskId){
     taskCard = document.getElementById("taskCards");
-    taskCard.innerHTML += ` <div class="col-12 col-sm-6 col-lg-4">
+    taskCard.innerHTML += ` <div class="col-12 col-sm-6 col-lg-4" id="taskID-${taskId}">
     <div class="list-group">
         <div  class="list-group-item list-group-item-action ">
           <div class="d-flex w-100 justify-content-between">
@@ -120,24 +126,46 @@ function validateTaskForm(assignedBy, description, assignedTo, dueDate){
 };
 
 // Creates a summarry card using the data stored in local storage
-function createTaskSummary(assignedTo, dueDate, status){
+function createTaskSummary(assignedTo, dueDate, status, taskId){
     summaryCard = document.getElementById("taskSummaryCard");
-    summaryCard.innerHTML += `<a href="#" class="list-group-item list-group-item-action ">
+    summaryCard.innerHTML += `<a href="#taskID-${taskId}" class="list-group-item list-group-item-action ">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">Task for ${assignedTo}</h5>
       <small>Due ${dueDate}</small>
     </div>
     <p class="mb-1">${status}</p>
-  </a>`
+  </a>`;
 }
 
 // Iterates over the the value for the key 'taskArray' after it's converted from a string into an array and then runs the 'createTaskSummary' and 'createTaskCard' functions with the the sliced array values passed through the function 
 function displayCards(){
   let list = JSON.parse(localStorage.getItem("taskArray"));
   for (i in list){
-    createTaskSummary(list[i].taskAssignedTo, list[i].taskDueDate, list[i].taskStatus);
-    createTaskCard(list[i].taskAssignedBy, list[i].taskDescription, list[i].taskAssignedTo, list[i].taskDueDate, list[i].taskStatus);
+    createTaskSummary(list[i].taskAssignedTo, list[i].taskDueDate, list[i].taskStatus, list[i].taskID);
+    createTaskCard(list[i].taskAssignedBy, list[i].taskDescription, list[i].taskAssignedTo, list[i].taskDueDate, list[i].taskStatus, list[i].taskID);
   }
 }
 
-displayCards() //Calls the displayCards function so that anything in local storage under the key 'taskArray' with be presented on the web page as the in the form of the respective cards when the page loads.
+class TaskManager {
+  constructor(){
+    this.tmID = 0;
+
+  }
+  getAllTasks(){
+    
+  }
+
+  addTask(task){
+    this.tmID++
+  }
+
+  deleteTask(task){
+
+  }
+
+  updateTaskStatus(taskID, status){
+
+  }
+}
+
+displayCards(); //Calls the displayCards function so that anything in local storage under the key 'taskArray' with be presented on the web page as the in the form of the respective cards when the page loads.
